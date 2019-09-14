3af93eb64c0dd0625dd57a72679251426b059a7b
Saussereau Dominique <domsau2@yahoo.fr>
2019-09-14 04:21:53 +0200

    + min

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include <time.h>
 
 using namespace std;
-int dice ( int max = 6 )
+int dice ( int max = 6 , int min = 1)
 {
-    int min = 1;
-    int number = rand() % max + min; //
+     if ( max < min ) { // Usually, the minimum is before the maximum, and sometimes it is not the default value.
+        auto temp = max;
+        max = min;
+        min = temp;
+    }   
+    int number = rand() % (max - min + 1) + min;
     return number;
 }
 
