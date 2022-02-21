11bbd10b9fb8c88de6db6f872323979ef1d643a1
Saussereau Dominique <domsau2@yahoo.fr>
2022-02-21 19:51:41 +0100

    Better random.
    C++11 minimum.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
-#include <time.h>
-
+#include <random>
 using namespace std;
-int dice ( int max = 6 , int min = 1)
+
+int dice ( int RandMAX = 6 , int RandMIN = 1)
 {
-     if ( max < min ) { // Usually, the minimum is before the maximum, and sometimes it is not the default value.
-        auto temp = max;
-        max = min;
-        min = temp;
-    }   
-    int number = rand() % (max - min + 1) + min;
+     if ( RandMAX < RandMIN ) { // Usually, the minimum is before the maximum, and sometimes it is not the default value.
+        swap(RandMIN, RandMAX);
+    }
+    random_device dev;
+    mt19937 rng(dev());
+    uniform_int_distribution<std::mt19937::result_type> randInt(RandMIN, RandMAX);
+    int number = randInt(dev);
     return number;
 }
 
 int main()
 {
-    srand ( time ( NULL ) ); // No need for better init.
     for ( int i = 1; i <= 42; i++ ) // iMax inclus.
         {
         cout << dice ( 2 ) << " " << dice() << endl;
